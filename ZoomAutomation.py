685f2f6a03e611ec720357c0174351ac0f0936f5
engineerDavid <33636080+engineerDavid@users.noreply.github.com>
2021-04-09 15:40:02 -0500

    Updated the comments on file

--- ZoomAutomation.py
+++ ZoomAutomation.py
@@ -2,12 +2,16 @@ import webbrowser
 import datetime
 import time
 from datetime import datetime
+from dateutil import parser
 #add her your time and day of the week your zoom meeting is on 
 #0 = Monday
 #1= Tuesday
 #2 = Wednesday
 #3 = Thursday 
 #4 = Friday
+
+#Where you see ZoomUrl you need to add in your ZoomUrl link
+# you also need to add in the time and day of week for you classes
 ZoomClasss = {
     "classes": [
         {
@@ -40,37 +44,47 @@ ZoomClasss = {
     ]
 
 }
+
+
+#used to check what day it is of the week 0-6
+#monday = 0
 weekday = datetime.today().weekday()
 t = time.localtime()
+#gets the current time at your location
 current_time = time.strftime("%H:%M", t)
+#used to only have hours and min format
 FMT = '%H:%M'
-ComingUpClasses =[]
-#print(c[1]["Day of week"][1])
-from dateutil import parser
+#23 hours is used since you dont usualyy open zoom 23 hours before class
 closestClass = "23:00:00"
+
 for c in ZoomClasss.values():
+    #used to go through every class in dic
     for clas in c:
+        #goes through the days of the week your class is on
         for day in clas["Day of week"]:
+            #check to see what classes you have today
             if weekday == day:
+                #gets the time of todays classes
                 ClassTime = clas["Time"]
-                #FMT = '%H:%M'
-                
+                #checks to see how much time you hvae left before your classes for today start
                 tdelta = datetime.strptime(ClassTime, FMT) - datetime.strptime(current_time, FMT)
-                #time = datetime.strptime(tdelta, FMT)
-                
+                #makes sure your next class is not a day away
                 if str(tdelta).find("day") == -1:
+                    #parser helps compare the times
                     tdelta = parser.parse(str(tdelta))
                     closestClass = parser.parse(str(closestClass))
-                    
+                    #used to see which class is happening sooner
                     if tdelta < closestClass:
-                        print(tdelta)
                         closestClass = tdelta
+                        #keeps track of the url of the class with closer time
                         ZoomUrl = clas["ZoomUrl"]
                 else:
                     break
-print(ZoomUrl)
 
 
+
+#Used to open up the url in the chrome browser
+#if you brefer a diffrent browser just change the 'chrome' in code bellow to desired code
 webbrowser.register('chrome',
 	None,
 	webbrowser.BackgroundBrowser("C://Program Files (x86)//Google//Chrome//Application//chrome.exe"))
